6fce92505403668306f749b7d1ead2c11be06982
Huangwenzhou <418312660@qq.com>
2020-09-17 16:41:58 +0800

    first commit

---
+++ application/extra/addons.php
@@ -0,0 +1,47 @@
+<?php
+
+return array (
+  'autoload' => false,
+  'hooks' => 
+  array (
+    'app_init' => 
+    array (
+      0 => 'cms',
+    ),
+    'view_filter' => 
+    array (
+      0 => 'cms',
+    ),
+    'user_sidenav_after' => 
+    array (
+      0 => 'cms',
+    ),
+    'xunsearch_config_init' => 
+    array (
+      0 => 'cms',
+    ),
+    'xunsearch_index_reset' => 
+    array (
+      0 => 'cms',
+    ),
+    'config_init' => 
+    array (
+      0 => 'nkeditor',
+    ),
+  ),
+  'route' => 
+  array (
+    '/$' => 'cms/index/index',
+    '/t/[:name]$' => 'cms/tags/index',
+    '/p/[:diyname]$' => 'cms/page/index',
+    '/s$' => 'cms/search/index',
+    '/d/[:diyname]' => 'cms/diyform/index',
+    '/special/[:diyname]' => 'cms/special/index',
+    '/a/[:diyname]$' => 'cms/archives/index',
+    '/c/[:diyname]$' => 'cms/channel/index',
+    '/u/[:id]' => 'cms/user/index',
+  ),
+  'priority' => 
+  array (
+  ),
+);
